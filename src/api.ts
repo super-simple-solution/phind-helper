804c418be98a7c125c29d0e79528f9e8538eedca
wuxue <wuxue@windimg.com>
2023-03-04 03:11:50 +0800

    api

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,35 @@
 import service from '@/utils/fetch'
 
+const phindApiPrefix = 'https://phind.com/api/'
 const apiMap = {
-  suggestions: 'https://phind.com/api/suggestions',
+  suggestions: `${phindApiPrefix}suggestions`,
+  search: `${phindApiPrefix}search`,
+  concise: `${phindApiPrefix}concise`,
 }
 
 export function getSuggestions(params: { [char: string]: string }): Promise<any> {
   return service(apiMap.suggestions, { params })
+}
+export function search(params: any): Promise<any> {
+  return service.post(apiMap.search, {
+    userRankList: {
+      '.rs': 1,
+      'developer.mozilla.org': 1,
+      'en.wikipedia.org': 1,
+      'github.com': 1,
+      'rust-lang': 2,
+      'stackoverflow.com': 1,
+      'www.amazon.com': -1,
+      'www.pinterest.com': -3,
+      'www.quora.com': -2,
+      'www.reddit.com': 1,
+    },
+    browserLanguage: 'en',
+    freshness: '',
+    q: params.q,
+  })
+}
+
+export function concise(params: any): Promise<any> {
+  return service.post(apiMap.concise, ...params)
 }
